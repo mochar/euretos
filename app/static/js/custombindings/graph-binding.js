4c54cc1d4a4168b9b58e64265f7d63c163b4f842
Mo Char <mocharrout@gmail.com>
2016-08-04 20:13:58 +0200

    Tweaked graph behaviour

--- app/static/js/custombindings/graph-binding.js
+++ app/static/js/custombindings/graph-binding.js
@@ -46,8 +46,8 @@ ko.bindingHandlers.graph = {
             nodeHeight = 20;
             
         var simulation = d3.forceSimulation()
-            .force('link', d3.forceLink().id(function(d) { return d.id; }))
-            .force('charge', d3.forceManyBody())
+            .force('link', d3.forceLink().id(function(d) { return d.id; }).distance(100))
+            .force('charge', d3.forceManyBody().strength(-40))
             .force('center', d3.forceCenter(width / 2, height / 2));
         
         var link = svg.select('g.links').selectAll('.link')
