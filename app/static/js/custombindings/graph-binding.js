2a5e3161d062920d000aa4c7747545034a5dd5b0
Mo Char <mocharrout@gmail.com>
2016-08-07 21:28:31 +0200

    Turned link width scale to linear one

--- app/static/js/custombindings/graph-binding.js
+++ app/static/js/custombindings/graph-binding.js
@@ -78,7 +78,7 @@ ko.bindingHandlers.graph = {
             defs = svg.select('defs'),
             g = svg.select('g'),
             nodeHeight = 20,
-            linkWidth = d3.scaleLog()
+            linkWidth = d3.scaleLinear()
                 .domain([1, publicationMax])
                 .range([2, 10]);
         
